97d1705e8a16c37da6ea3a184b6e3520a28fdf7a
Caryolite <carl47063@gmail.com>
2024-08-15 03:36:40 +0800

    feat: 新增首頁遊戲截圖圖片切換功能, 修改部分圖片

---
+++ script.js
@@ -0,0 +1,43 @@
+const scr1 = document.querySelector(".scr-1");
+const scr2 = document.querySelector(".scr-2");
+const scr3 = document.querySelector(".scr-3");
+const scr4 = document.querySelector(".scr-4");
+const scr5 = document.querySelector(".scr-5");
+
+let scrb = [
+    document.querySelector(".scr-1b"),
+    document.querySelector(".scr-2b"),
+    document.querySelector(".scr-3b"),
+    document.querySelector(".scr-4b"),
+    document.querySelector(".scr-5b")
+]
+
+let p = 2;
+
+function changeImg(){
+    // 測試
+    // console.log("c");
+    // console.log(scrb[p]);
+
+    // 把不用的圖片隱藏
+    var x = document.querySelectorAll(".scrb");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.visibility = 'hidden';
+    } 
+
+    // 把點擊的圖片顯示
+    scrb[p].style.visibility = 'visible';
+}
+
+scr1.addEventListener("click",() => p = 0);
+scr2.addEventListener("click",() => p = 1);
+scr3.addEventListener("click",() => p = 2);
+scr4.addEventListener("click",() => p = 3);
+scr5.addEventListener("click",() => p = 4);
+
+scr1.addEventListener("click",changeImg);
+scr2.addEventListener("click",changeImg);
+scr3.addEventListener("click",changeImg);
+scr4.addEventListener("click",changeImg);
+scr5.addEventListener("click",changeImg);
